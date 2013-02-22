c699350ed3920ee7d5695c12fafd70e5f8b819de
Yaron Minsky <yminsky@janestreet.com>
2013-02-21 19:06:44 -0500

    added short-paths flag

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ eval `opam config -env`
 
 for TARGET in $*
 do
-  ocamlbuild -use-ocamlfind $TARGET -cflags "-w @A-4-33-23"
+  ocamlbuild -use-ocamlfind $TARGET -cflags "-w @A-4-33-23" -cflags -short-paths
 done
 
 
