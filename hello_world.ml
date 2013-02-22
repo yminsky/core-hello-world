ab95b13a3b134c74deebeb33f26f2618713b945b
Yaron Minsky <yminsky@janestreet.com>
2013-02-21 22:27:21 -0500

    more comments

--- hello_world.ml
+++ hello_world.ml
@@ -1,8 +1,15 @@
 open Core.Std
 
+(* A very basic command-line program, using Command, Core's Command line
+   parsing library.  *)
+
 let command =
+  (* [Commabd.basic] is used for creating a command.  Every command takes a text
+     summary and a command line spec *)
   Command.basic
     ~summary:"Hello World"
+    (* Command line specs are built up component by component, using a small
+       combinator library whose operators are contained in [Command.Spec] *)
     Command.Spec.(
       empty
       +> flag "-hello" (optional_with_default "Hello" string)
@@ -10,9 +17,13 @@ let command =
       +> flag "-world" (optional_with_default "World" string)
         ~doc:" The 'world' of 'hello world'"
     )
+    (* The command-line spec determines the argument to this function, which
+       show up in an order that matches the spec. *)
     (fun hello world () ->
       printf "%s %s!\n" hello world
     )
 
 let () =
-  Command.run command
+  (* Exn.handle_uncaught is here to improve how exceptions are printed out when
+     the program exits. *)
+  Exn.handle_uncaught ~exit:true (fun () -> Command.run command)
---
+++ hello_world.mli
@@ -0,0 +1,2 @@
+(* The mli is here, and empty, so that the compiler will warn us about any
+   unused functions in the body.  *)
