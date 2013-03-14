e226b38e0f7ee2a3b1649001968542b0357b92d2
Yaron Minsky <yminsky@janestreet.com>
2013-03-13 22:06:05 -0400

    update to Core 109.13

--- common.ml
+++ common.ml
@@ -17,7 +17,7 @@ let with_rpc_conn f ~host ~port =
   Tcp.with_connection
     (Tcp.to_host_and_port host port)
     ~timeout:(sec 1.)
-    (fun r w ->
+    (fun _ r w ->
       Rpc.Connection.create r w ~connection_state:()
       >>= function
       | Error exn -> raise exn
