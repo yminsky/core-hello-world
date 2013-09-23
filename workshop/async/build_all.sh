33988205f6dc4a6abd7e8a88e125a41c5f7f65a0
Yaron Minsky <yminsky@janestreet.com>
2013-09-22 23:12:44 -0400

    updates

--- workshop/async/build_all.sh
+++ workshop/async/build_all.sh
@@ -2,10 +2,10 @@
 
 EXT=native
 
-./build.sh \
-    simple.$EXT \
-    time_loop.$EXT \
-    manifest.$EXT \
-    pipe_manifest.$EXT \
-    count_lines.$EXT \
+corebuild \
+    simple.native \
+    time_loop.native \
+    manifest.native \
+    pipe_manifest.native \
+
 
--- workshop/async/simple.ml
+++ workshop/async/simple.ml
@@ -1,13 +1,15 @@
 open Core.Std
 open Async.Std
 
-let def =
+let (_:unit Deferred.t) =
   after (sec 0.5)
   >>= fun () ->
   printf "Hello ";
   after (sec 1.)
   >>= fun () ->
   printf "world!\n";
+  after (sec 2.)
+  >>= fun () ->
   shutdown 0;
   return ()
 
