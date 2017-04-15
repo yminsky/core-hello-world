1f9fb2751f0df3eee305954b8f50e9a7400df781
Yaron Minsky <yminsky@janestreet.com>
2017-04-15 17:00:24 -0400

    simplified build script

--- src/build.sh
+++ src/build.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-EXT=exe
-
-jbuilder build --dev \
-    hello_world.$EXT \
-    hello_client.$EXT \
-    hello_server.$EXT \
-    broker_server.$EXT \
-    broker_client.$EXT \
+jbuilder build --dev @install
 
